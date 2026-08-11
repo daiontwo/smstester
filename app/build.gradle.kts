8ee888b68056767c2886e5b747621103bf3eb093
daiontwo <daiontwo@gmail.com>
2026-08-11 07:35:35 +0300

    v1.3 Config "versionCode": 4,

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,8 @@
 plugins {
+    id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.gms.google-services")
-    id("com.android.application")
-}
-
-android {
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-    }
 }
 
 android {
@@ -26,27 +13,41 @@ android {
         applicationId = "com.antteam.smstester"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
     }
 
-    buildFeatures { compose = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
-    kotlinOptions { jvmTarget = "17" }
+    buildFeatures {
+        compose = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        )
+    }
 }
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
+
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-functions")
-    implementation("androidx.fragment:fragment-ktx:1.8.5")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
-
-
